7c25c469f338dc8a18ce4893fa8bfba054c032c5
ClaysonEufrasio <claysonrobertoeufrasio@gmail.com>
2022-03-04 15:52:30 -0300

    continue

---
+++ Exercicios-JS/Fundamentos/tipagem-fraca.js
@@ -0,0 +1,13 @@
+let qualquer = "legal"
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.15
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evitar nome genérico e sigla
+
+let valor = ''
+let numero = 1
+let pqp = false // Produto Químico Perigoso
---
+++ Exercicios-JS/Fundamentos/variaveis-e-contantes.js
@@ -0,0 +1,16 @@
+var a = 3
+let b = 4
+
+var a = 30 // var PODE ser declarada mais de uma vez no mesmo escopo
+b = 40 // let NÃO pode ser declarada mais de uma vez no mesmo escopo
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5 // c = 50 cont NÃO pode ser alterada
+
+console.log(c)
