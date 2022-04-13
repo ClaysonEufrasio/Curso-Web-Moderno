9a39061cbe09fe60f54f18f3cc5fc77817fa8bee
ClaysonEufrasio <claysonrobertoeufrasio@gmail.com>
2022-04-13 08:44:39 -0300

    Create operadoresTernarios.js

---
+++ Exercicios-JS/Fundamentos/operadoresTernarios.js
@@ -0,0 +1,7 @@
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+console.log(resultado(7.1))
+
+const soma = (nota1, nota2) => nota1 < nota2 ? 'Nota 2 maior' : 'Nota 1 maior'
+
+console.log(soma(7, 3))
