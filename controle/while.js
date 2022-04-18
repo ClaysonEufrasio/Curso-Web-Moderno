881964ce473980ce4b2119e5b5f0cd65a8a6b60f
ClaysonEufrasio <claysonrobertoeufrasio@gmail.com>
2022-04-18 12:39:33 -0300

    Create while.js

---
+++ controle/while.js
@@ -0,0 +1,11 @@
+function inteiroAleatorio(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = 0
+while (opcao != -1) {
+    opcao = inteiroAleatorio(-1, 10)
+    console.log(`Opção escolida ${opcao}`)
+}
+
