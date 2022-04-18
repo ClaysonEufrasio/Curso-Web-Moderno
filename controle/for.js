55cd8065ee907b9842ad62f1e7486f4d9cabecec
ClaysonEufrasio <claysonrobertoeufrasio@gmail.com>
2022-04-18 12:56:40 -0300

    Create for.js

---
+++ controle/for.js
@@ -0,0 +1,17 @@
+let contatdor = 1
+
+while (contatdor <= 10) {
+    console.log(`Contador = ${contatdor}`)
+    contatdor++
+}
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`I = ${i}`)
+}
+
+const notas = [6, 5, 2, 8, 6, 5]
+for (let i = 0; i < notas.length; i++) {
+    console.log(notas[i])
+}
+
